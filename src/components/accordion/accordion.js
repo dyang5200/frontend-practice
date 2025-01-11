ca70e987b18f702a13379aa08c80e23c9643bfe8
Danielle Yang <danielleyang@Danielles-MacBook-Air.local>
2025-01-11 14:22:33 -0800

    Abstract

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 import './accordion.css';
 
+const sections = [{
+    title: "HTML",
+    content: <div>The HyperText Markup Language or HTML is the
+        standard markup language for documents designed to
+        be displayed in a web browser.</div>
+}, {
+    title: "CSS",
+    content: <div>Cascading Style Sheets is a style sheet language
+        used for describing the presentation of a document
+        written in a markup language such as HTML or XML.</div>
+}, {
+    title: "JavaScript",
+    content: <div>JavaScript, often abbreviated as JS, is a
+        programming language that is one of the core
+        technologies of the World Wide Web, alongside HTML
+        and CSS.</div>
+}]
+
 export default function Accordion() {
     return (
         <div>
-            <Section title="HTML" content={<div>The HyperText Markup Language or HTML is the
-                standard markup language for documents designed to
-                be displayed in a web browser.</div>} />
-            <Section title="CSS" content={<div>Cascading Style Sheets is a style sheet language
-                used for describing the presentation of a document
-                written in a markup language such as HTML or XML.</div>} />
-            <Section title="JavaScript" content={<div>JavaScript, often abbreviated as JS, is a
-                programming language that is one of the core
-                technologies of the World Wide Web, alongside HTML
-                and CSS.</div>} />
+            {sections.map((section, index) => {
+                return <Section title={section.title} content={section.content} key={index} />
+            })}
         </div>
     );
 }
