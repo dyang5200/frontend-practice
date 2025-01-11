26402d8edf044c0c4c83b26dc2f6547c4882ecae
Danielle Yang <danielleyang@Danielles-MacBook-Air.local>
2025-01-11 14:10:26 -0800

    Fixed button appearance

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
-import Section from './components/section';
 import './accordion.css';
 
 export default function Accordion() {
-    // let section = ["HTML", "CSS", "JavaScript"]
-
     return (
         <div>
             <Section title="HTML" content={<div>The HyperText Markup Language or HTML is the
@@ -17,6 +14,26 @@ export default function Accordion() {
                 programming language that is one of the core
                 technologies of the World Wide Web, alongside HTML
                 and CSS.</div>} />
+        </div>
+    );
+}
+
+
+function Section({ title, content }) {
+    const [isShown, setIsShown] = useState(false);
+
+    return (
+        <div>
+            <div>
+                {title}{" "}
+                <button onClick={() => setIsShown(!isShown)}>
+                    <span
+                        aria-hidden={isShown}
+                        className={"accordion-icon" + (isShown ? "" : "--rotated")}
+                    />
+                </button>
+            </div>
+            <div>{isShown && content}</div>
         </div>
     );
 }
